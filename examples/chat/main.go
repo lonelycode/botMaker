1e2b87e2f610aa625f3b8643f617262dc7ffc4fc
Martin Buhr <martin@Martins-Air.lan>
2023-04-24 22:09:43 +1200

    added some comments to example

--- examples/chat/main.go
+++ examples/chat/main.go
@@ -10,21 +10,38 @@ import (
 )
 
 func main() {
+	// For the typewriter, not important
 	var min int64 = 5
 	var max int64 = 30
 
+	// Get the system config (API keys and Pinecone endpoint)
 	cfg := botMaker.NewConfigFromEnv()
+
+	// Set up the OAI API client
 	oai := botMaker.NewOAIClient(cfg.OpenAPIKey)
+
+	// Get the tuning for the bot, we'll use some defaults
 	settings := botMaker.NewBotSettings()
+
+	// We set the ID for the bot as this will be used when querying
+	// pinecone for context embeddings specifically for this bot -
+	// use different IDs for difference PC namespaces to create
+	// different context-flavours for bots
 	settings.ID = "a45dbe63-4207-419c-bca7-5d940bf3d908"
 
-	// For adding context, you can attach a memory store to query
+	// If adding context (additional data outside of GPTs training data), y
+	// you can attach a memory store to query
 	settings.Memory = &botMaker.Pinecone{
 		APIEndpoint: cfg.PineconeEndpoint,
 		APIKey:      cfg.PineconeKey,
 	}
 
+	// the Prompt holds all the information and logic needed to make a query to OpenAI,
+	// to change the way the prompt is presented to the AI, provide a text/template
+	// (see DEFAULT_TEMPLATE in botMaker.go for the default).
 	prompt := botMaker.NewBotPrompt("", oai)
+
+	// Set an initial instruction to the bot
 	prompt.Instructions = "You are an AI chatbot that is funny and helpful"
 
 	Typewriter("Hi, I'm Globutron, your friendly neighborhood chatbot powered by GPT3. Let's chat!", min, max)
@@ -34,16 +51,20 @@ func main() {
 		fmt.Print("\nInput: ")
 		text, _ := reader.ReadString('\n')
 
+		// We populate the Body with the query from the user
 		prompt.Body = text
 		if text == "quit\n" {
 			break
 		}
 
+		// make the OpenAI query, the prompt object will render the query
+		// according to its template with the context embeddings pulled from Pinecone
 		resp, _, err := oai.CallUnifiedCompletionAPI(settings, prompt)
 		if err != nil {
 			fmt.Println(err)
 		}
 
+		// Show the response
 		Typewriter("\n"+resp, min, max)
 
 		oldBody := "Human: " + prompt.Body
